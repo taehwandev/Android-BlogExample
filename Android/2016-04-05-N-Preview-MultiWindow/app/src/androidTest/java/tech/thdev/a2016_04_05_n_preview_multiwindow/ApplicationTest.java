1d26c2a8481efeb7c11881b8a043a1302e2f1816
Taehwan <taehwan@thdev.net>
2016-04-07 14:01:28 +0900

    Android Blog example commit
    N Preview Multiwindow example
    BottomSheet example

---
+++ Android/2016-04-05-N-Preview-MultiWindow/app/src/androidTest/java/tech/thdev/a2016_04_05_n_preview_multiwindow/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tech.thdev.a2016_04_05_n_preview_multiwindow;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
