71b2ca9a9a5ea59b4807f195209c12576cce1a03
Taehwan <taehwan@thdev.net>
2016-04-20 18:04:45 +0900

    New Projection
    AndroidTest Example.

---
+++ Android/2016-04-20-AndroidTestExample/app/src/main/java/tech/thdev/androidtestexample/SecondActivity.java
@@ -0,0 +1,28 @@
+package tech.thdev.androidtestexample;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import tech.thdev.androidtestexample.base.BaseActivity;
+import tech.thdev.androidtestexample.constant.Constant;
+
+/**
+ * Created by Tae-hwan on 4/20/16.
+ */
+public class SecondActivity extends BaseActivity {
+
+    @Bind(R.id.tv_message)
+    TextView textView;
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_second;
+    }
+
+    @Override
+    protected void onCreate() {
+        Log.d("TAG", "getIntent().getStringExtra(Constrant.INTENT_MESSAGE) " + getIntent().getStringExtra(Constant.INTENT_MESSAGE));
+        textView.setText(getIntent().getStringExtra(Constant.INTENT_MESSAGE));
+    }
+}
---
+++ Android/2016-04-20-AndroidTestExample/app/src/main/java/tech/thdev/androidtestexample/constant/Constant.java
@@ -0,0 +1,10 @@
+package tech.thdev.androidtestexample.constant;
+
+/**
+ * Created by Tae-hwan on 4/20/16.
+ */
+public class Constant {
+
+    public static final String INTENT_MESSAGE = "message";
+    public static final String INTENT_URL = "url";
+}
