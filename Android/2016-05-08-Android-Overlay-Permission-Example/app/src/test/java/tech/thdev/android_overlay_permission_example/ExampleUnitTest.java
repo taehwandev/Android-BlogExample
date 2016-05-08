100e476289f4f198fb06d1eed9ebab2e34da1b38
TaeHwan <taehwan@thdev.net>
2016-05-08 15:28:14 +0900

    New example Overlay permission

---
+++ Android/2016-05-08-Android-Overlay-Permission-Example/app/src/test/java/tech/thdev/android_overlay_permission_example/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package tech.thdev.android_overlay_permission_example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
