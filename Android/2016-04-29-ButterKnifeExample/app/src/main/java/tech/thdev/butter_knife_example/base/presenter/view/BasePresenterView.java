59ed94c7a1c7c93c3bb254da3009cd8309dc0164
Taehwan <taehwan@thdev.net>
2016-07-25 11:41:27 +0900

    Android Sample change
    Android sample로 변경

--- Android/2016-04-29-ButterKnifeExample/app/src/main/java/tech/thdev/butter_knife_example/base/presenter/view/BasePresenterView.java
+++
@@ -1,8 +0,0 @@
-package tech.thdev.butter_knife_example.base.presenter.view;
-
-/**
- * Created by Tae-hwan on 4/26/16.
- */
-public interface BasePresenterView {
-
-}
--- Android/2016-04-29-ButterKnifeExample/app/src/main/java/tech/thdev/butter_knife_example/network/domain/RecentPhotoResponse.java
+++
@@ -1,6 +0,0 @@
-package tech.thdev.butter_knife_example.network.domain;
-
-public class RecentPhotoResponse {
-
-    public PhotosPageInfo photos;
-}
