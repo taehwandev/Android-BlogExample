6710ab49ec3a15bf0d548c7329856893469c7b26
Taehwan <taehwan@thdev.net>
2016-08-04 21:13:18 +0900

    Project - Version update : constraintLayoutVersion = '1.0.0-alpha5'
    New Sample add.
     Android WebView Sample update.

---
+++ CustomWebView/src/main/java/tech/thdev/customwebview/webkit/CustomWebChromeClient.java
@@ -0,0 +1,80 @@
+package tech.thdev.customwebview.webkit;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+/**
+ * Created by Tae-hwan on 8/4/16.
+ */
+
+public class CustomWebChromeClient extends WebChromeClient {
+
+    private Activity activity;
+
+    public CustomWebChromeClient(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+        AlertDialog alertDialog = new AlertDialog.Builder(activity).setMessage(message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                result.confirm();
+            }
+        }).setCancelable(false).create();
+
+        alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                result.cancel();
+            }
+        });
+
+        if (activity.isFinishing()) {
+            result.confirm();
+        } else {
+            alertDialog.show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+        AlertDialog alertDialog = new AlertDialog.Builder(activity).setMessage(message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                result.confirm();
+            }
+        }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                result.cancel();
+            }
+        }).setCancelable(false).create();
+
+        alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                result.cancel();
+            }
+        });
+
+        if (activity.isFinishing()) {
+            result.cancel();
+
+        } else {
+            alertDialog.show();
+        }
+
+        return true;
+    }
+}
