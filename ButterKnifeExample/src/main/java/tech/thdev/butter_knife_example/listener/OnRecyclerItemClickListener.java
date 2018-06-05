cf6aacb051985d4609394146878b6cc7efff76b7
Taehwan <taehwan@thdev.net>
2018-06-05 14:32:25 +0900

    Create default studio. Apply package androidx. app only. only kotlin.

--- ButterKnifeExample/src/main/java/tech/thdev/butter_knife_example/listener/OnRecyclerItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package tech.thdev.butter_knife_example.listener;
-
-/**
- * Created by Tae-hwan on 4/26/16.
- */
-public interface OnRecyclerItemClickListener {
-
-    void onItemClick(int position);
-}
---
+++ app/src/main/java/tech/thdev/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package tech.thdev.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tech.thdev.app.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
