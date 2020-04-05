54eddc73e43009dc800b30f81d7576de9d686135
Taehwan <taehwan@thdev.net>
2020-04-05 23:12:25 +0900

    암호화 파일 쓰기

--- app/src/main/java/tech/thdev/app/ui/main/MainFragment.kt
+++ app/src/main/java/tech/thdev/app/ui/main/MainFragment.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.security.crypto.EncryptedFile
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
+import kotlinx.coroutines.flow.callbackFlow
 import tech.thdev.app.databinding.MainFragmentBinding
 import java.io.File
 
@@ -24,6 +27,8 @@ class MainFragment : Fragment() {
     private val sharedPreferences: SharedPreferences by lazy {
         val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
         val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+        android.util.Log.e("TEMP", "masterKeyAlias ${masterKeyAlias}")
 
         EncryptedSharedPreferences.create(
             "security_test",
@@ -51,33 +56,63 @@ class MainFragment : Fragment() {
                 .apply()
         }
 
+        val biometricPrompt = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .setConfirmationRequired(true)
+            .build()
+
+        val listener = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                android.util.Log.i("TEMP", "Unlock error errorCode $errorCode errString $errString")
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                // Unlocked
+                android.util.Log.i("TEMP", "Unlock success")
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                android.util.Log.i("TEMP", "Unlock failed")
+            }
+        }
+        val biometric = BiometricPrompt(
+            requireActivity(),
+            ContextCompat.getMainExecutor(requireContext()),
+            listener
+        )
+
         binding.btnFileExample.setOnClickListener {
             // Although you can define your own key generation parameter specification, it's
             // recommended that you use the value specified here.
-//            val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
-//            val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
-//
-//            val fileToRead = "my_sensitive_data.txt"
-//            val file = File(requireContext().cacheDir, fileToRead)
-//            if (file.exists()) {
-//                file.delete()
-//            }
-//            val encryptedFile = EncryptedFile.Builder(
-//                file,
-//                requireContext(),
-//                "masterKeyAlias--aaaa",
-//                EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
-//            ).build()
-//
-//            encryptedFile.openFileOutput().bufferedWriter().use {
-//                it.write(binding.editQuery.text.toString())
-//            }
-//
-//            val content = encryptedFile.openFileInput().bufferedReader().useLines { lines ->
-//                lines.fold(" ") { working, line ->
-//                    "$working $line"
-//                }
-//            }
+            val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+            val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+            val fileToRead = "my_sensitive_data.txt"
+            val file = File(requireContext().cacheDir, fileToRead)
+            if (file.exists()) {
+                file.delete()
+            }
+            val encryptedFile = EncryptedFile.Builder(
+                file,
+                requireContext(),
+                "masterKeyAlias--aaaa",
+                EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+            ).build()
+
+            encryptedFile.openFileOutput().bufferedWriter().use {
+                it.write(binding.editQuery.text.toString())
+            }
+
+            val content = encryptedFile.openFileInput().bufferedReader().useLines { lines ->
+                lines.fold(" ") { working, line ->
+                    "$working $line"
+                }
+            }
 
 
 //            binding.tvShowQuery.text = content
