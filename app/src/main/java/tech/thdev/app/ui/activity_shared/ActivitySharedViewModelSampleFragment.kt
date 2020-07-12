6e00267d6e1889181197ed3bcda9475c3be5b1b9
Taehwan <taehwan@thdev.net>
2020-07-12 22:26:41 +0900

    New sample - Activity/Fragment use Shared ViewModel

--- app/src/main/java/tech/thdev/app/ui/activity_shared/ActivitySharedViewModelSampleFragment.kt
+++ app/src/main/java/tech/thdev/app/ui/activity_shared/ActivitySharedViewModelSampleFragment.kt
@@ -5,11 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import tech.thdev.app.R
 import tech.thdev.app.databinding.ActivitySharedFragmentBinding
+import tech.thdev.app.ui.MainViewModel
 
 class ActivitySharedViewModelSampleFragment : Fragment() {
+
+    private val viewModel: ActivitySharedViewModelSampleViewModel by viewModels()
+
+//    private val activityViewModel: MainViewModel by lazy {
+//        ViewModelProvider(requireActivity(), object : ViewModelProvider.Factory {
+//            override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+//                MainViewModel() as T
+//        }).get(MainViewModel::class.java)
+//    }
+    private val activityViewModel: MainViewModel by activityViewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+                MainViewModel() as T
+        }
+    }
 
     private var _binding: ActivitySharedFragmentBinding? = null
     private val binding: ActivitySharedFragmentBinding get() = _binding!!
@@ -26,9 +47,26 @@ class ActivitySharedViewModelSampleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnNext.setOnClickListener {
+        viewModel.updateFragmentCount.observe(viewLifecycleOwner, Observer {
+            binding.tvMessage.text = getString(R.string.default_message_view, it)
+        })
+
+        binding.initView()
+    }
+
+    private fun ActivitySharedFragmentBinding.initView() {
+        btnInsertCoinFragment.setOnClickListener {
+            viewModel.plus()
         }
-        binding.btnPrevious.setOnClickListener {
+
+        btnInsertCoinActivity.setOnClickListener {
+            activityViewModel.insertCountTwo()
+        }
+
+        btnNext.setOnClickListener {
+
+        }
+        btnPrevious.setOnClickListener {
             findNavController().navigate(R.id.action_previous_activitySharedViewModel_to_DefaultViewModelFragment)
         }
     }
