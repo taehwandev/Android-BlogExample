4fd82bd1e84d206a0230ad01a23b48a4685931aa
taehwan <taehwan@thdev.net>
2021-10-17 10:46:01 +0900

    Update build.gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,3 +6,10 @@ repositories {
     google()
     mavenCentral()
 }
+
+val kotlinVersion = "1.5.21"
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    languageVersion = kotlinVersion
+}
