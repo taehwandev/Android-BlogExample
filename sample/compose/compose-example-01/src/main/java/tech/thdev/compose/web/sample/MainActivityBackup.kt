1c1b4467ab18d5e9d6fcc892961abfa2ae92a3a6
taehwan <taehwan@thdev.net>
2024-07-27 11:06:38 +0900

    Update

--- sample/compose/compose-example-01/src/main/java/tech/thdev/compose/web/sample/MainActivityBackup.kt
+++ sample/compose/compose-example-01/src/main/java/tech/thdev/compose/web/sample/MainActivityBackup.kt
@@ -12,19 +12,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
@@ -36,6 +46,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
@@ -49,10 +60,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import tech.thdev.compose.web.sample.ui.holder.home.HomeScreenThree
 import tech.thdev.compose.web.sample.ui.holder.web.CustomWebChromeClient
 import tech.thdev.compose.web.sample.ui.holder.web.CustomWebViewClient
 import tech.thdev.compose.web.sample.ui.holder.web.LocalWebOwner
+import tech.thdev.compose.web.sample.ui.model.ListItem
 import tech.thdev.compose.web.sample.ui.model.NavigationSample
 import tech.thdev.compose.web.sample.ui.theme.MyApplicationTheme
 
@@ -155,7 +166,162 @@ private fun MainScreen(
                     exitTransition = { ExitTransition.None },
                 ) {
                     composable(route = NavigationSample.Trigger.HOME.name) {
-                        HomeScreenThree()
+                        var listItem by remember { mutableStateOf(ListItem(emptyList())) }
+                        Column {
+                            LazyColumn(
+                                contentPadding = PaddingValues(20.dp),
+                                verticalArrangement = Arrangement.spacedBy(10.dp),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            ) {
+                                items(listItem.items) { item ->
+                                    Surface(
+                                        shape = MaterialTheme.shapes.small,
+                                    ) {
+                                        if (item.editMode) {
+                                            Column(
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                                    .padding(16.dp)
+                                            ) {
+                                                var changeItem by remember { mutableStateOf(item) }
+                                                TextField(
+                                                    value = changeItem.text,
+                                                    onValueChange = { new ->
+                                                        changeItem = changeItem.copy(
+                                                            text = new,
+                                                        )
+                                                    },
+                                                    modifier = Modifier
+                                                        .fillMaxWidth()
+                                                )
+
+                                                Row {
+                                                    Button(
+                                                        onClick = {
+                                                            listItem = listItem.copy(
+                                                                items = listItem.items.map { listItem ->
+                                                                    if (listItem.index == item.index) {
+                                                                        changeItem.copy(
+                                                                            editMode = false,
+                                                                        )
+                                                                    } else {
+                                                                        listItem
+                                                                    }
+                                                                },
+                                                            )
+                                                        },
+                                                        modifier = Modifier
+                                                            .weight(1f)
+                                                    ) {
+                                                        Text(
+                                                            text = "Save",
+                                                        )
+                                                    }
+
+                                                    Button(
+                                                        onClick = {
+                                                            listItem = listItem.copy(
+                                                                items = listItem.items.toMutableList()
+                                                                    .also { newList ->
+                                                                        newList.remove(item)
+                                                                    },
+                                                            )
+                                                        },
+                                                        modifier = Modifier
+                                                            .weight(1f)
+                                                            .padding(start = 10.dp)
+                                                    ) {
+                                                        Text(
+                                                            text = "X",
+                                                        )
+                                                    }
+                                                }
+                                            }
+                                        } else {
+                                            Column(
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                                    .background(color = Color.Gray.copy(0.3f))
+                                            ) {
+                                                Row {
+                                                    Text(
+                                                        text = item.text,
+                                                        modifier = Modifier
+                                                            .weight(1f)
+                                                            .padding(horizontal = 16.dp)
+                                                            .padding(top = 16.dp)
+                                                    )
+
+                                                    IconButton(
+                                                        onClick = {
+                                                            listItem = listItem.copy(
+                                                                items = listItem.items.toMutableList()
+                                                                    .also { newList ->
+                                                                        newList.remove(item)
+                                                                    },
+                                                            )
+                                                        },
+                                                    ) {
+                                                        Icon(
+                                                            painter = painterResource(id = R.drawable.baseline_close_24),
+                                                            contentDescription = "remove",
+                                                        )
+                                                    }
+                                                }
+
+                                                Button(
+                                                    onClick = {
+                                                        listItem = listItem.copy(
+                                                            items = listItem.items.map { listItem ->
+                                                                if (listItem == item) {
+                                                                    listItem.copy(
+                                                                        editMode = true,
+                                                                    )
+                                                                } else {
+                                                                    listItem
+                                                                }
+                                                            },
+                                                        )
+                                                    },
+                                                    modifier = Modifier
+                                                        .fillMaxWidth()
+                                                        .padding(horizontal = 16.dp)
+                                                        .padding(top = 10.dp, bottom = 16.dp)
+                                                ) {
+                                                    Text(
+                                                        text = "edit",
+                                                    )
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            Button(
+                                onClick = {
+                                    listItem = listItem.copy(
+                                        items = listItem.items.toMutableList().also { list ->
+                                            list.add(
+                                                ListItem.Item(
+                                                    index = list.size,
+                                                    text = "",
+                                                    editMode = true,
+                                                )
+                                            )
+                                        },
+                                    )
+                                },
+                                modifier = Modifier
+                                    .padding(20.dp)
+                            ) {
+                                Text(
+                                    text = "New",
+                                )
+                            }
+                        }
                     }
 
                     composable(route = NavigationSample.Trigger.WEB.name) {
@@ -234,7 +400,8 @@ private fun MainScreen(
                                                     useWideViewPort = true
                                                     setSupportZoom(true)
 
-                                                    mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
+                                                    mixedContentMode =
+                                                        WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
                                                     setNetworkAvailable(true)
                                                     cacheMode = WebSettings.LOAD_DEFAULT
 
